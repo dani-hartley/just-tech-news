04f2e98da5a6256306b2c08c5705b3370ff24f68
dhartley <dani_hartley@msn.com>
2021-06-14 15:36:52 -0500

    hashed password

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # just-tech-news
 Module 13 ORM
+
+
+13.1 Completed
--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require ('../config/connection');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our User model
 
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
@@ -21,7 +22,7 @@ User.init(
         },
         //define a username column
         username: {
-            type: DataTypes.STRING, 
+            type: DataTypes.STRING,
             allowNull: false
         },
         // define an email column
@@ -29,10 +30,10 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             // there cannot be any duplicate email values in this table
-            unique: true, 
+            unique: true,
             // if allowNull is set to false, we can run our data through validators before creating the table
             validate: {
-                isEmail: true 
+                isEmail: true
             }
         },
         //define a password column
@@ -46,6 +47,19 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+
         // TABLE CONFIGURATIONS OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
@@ -58,6 +72,7 @@ User.init(
         // make it so our model name stays lowercase in the database
         modelName: 'user'
     }
+
 );
 
 module.exports = User;
